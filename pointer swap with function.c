a413f9285215d4b2b118078df0540a45b402e114
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-05-18 01:33:46 +0600

    feat: Implement pointer swap functionality in pointer-5.c

--- pointer swap with function.c
+++ pointer swap with function.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
+void swap(int *ptr1,int *ptr2)
+{
+
+int temp;
+temp=*ptr1;
+*ptr1=*ptr2;
+*ptr2=temp;
+
+
+}
+
 int main()
 {
 int x=20,y=90;
 
 printf("Before swap:\n X=%d\nY=%d",x,y);
-
-
+swap(&x,&y);
+printf("after swap:\n X=%d,Y=%d",x,y);
 }
