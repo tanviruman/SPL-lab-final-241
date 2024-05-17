48b4fceeebb0da6060f8e7b785bf1be9f1634956
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-05-18 01:12:11 +0600

    feat: Add pointer swap functionality in pointer-5.c

--- pointer-5.c
+++ pointer-5.c
@@ -1,1 +1,17 @@
 
+#include<stdio.h>
+int main()
+{
+int x=20,y=30,temp;
+int *ptr1,*ptr2;
+ptr1=&x;
+ptr2=&y;
+
+//swaping
+temp=*ptr1;
+*ptr1=*ptr2;
+*ptr2=temp;
+printf("X=%d , Y=%d",*ptr1,*ptr2);
+
+return 0;
+}
